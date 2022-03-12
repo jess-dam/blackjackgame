4db9eb7b207b919522286a70b717a5b91dc9ad6c
jess-dam <jess.dam@starlingbank.com>
2022-03-12 19:49:00 +0000

    Initial Commit

---
+++ blackjackgame/blackjackgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackjackgameApp.swift
+//  blackjackgame
+//
+//  Created by Jess Dam on 12/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct blackjackgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
